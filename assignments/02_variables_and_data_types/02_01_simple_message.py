e2964336f955b757f32431157f9b5a63e4f4dcc6
AlbrightBrianMedina <brian.medina001@albright.edu>
2026-09-01 19:39:44 -0400

    4th commit

--- assignments/02_variables_and_data_types/02_01_simple_message.py
+++ assignments/02_variables_and_data_types/02_01_simple_message.py
@@ -4,14 +4,6 @@ print(name.upper())
 print(name.lower())
 """
 
-
-first_name = "ada"
-last_name = "lovelace"
-full_name = f"{first_name} {last_name}"
-print(full_name)
-print(f"Hello, {full_name.title()}!")
-message = f"Hello, {full_name.title()}!"
-print(message)
-
-
-"""something something"""
+name = "My name is Brian Medina"
+print(name.upper())
+print(name.lower())
--- assignments/02_variables_and_data_types/02_02_simple_messages.py
+++ assignments/02_variables_and_data_types/02_02_simple_messages.py
@@ -1,5 +1,18 @@
-'''
+"""
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print(f"Hello, {full_name.title()}!")
+message = f"Hello, {full_name.title()}!"
+print(message)
+"""
 
-Even more
 
-'''
+first_name = "Brian"
+last_name = "Medina"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print(f"Greets there, {full_name.title()}!")
+message = f"Hello, {full_name.title()}!"
+print(message)
