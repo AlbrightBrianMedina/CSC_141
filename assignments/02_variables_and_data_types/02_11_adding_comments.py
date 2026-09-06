5274a1dce0da90e019b8d7e010f243ff1d852efe
AlbrightBrianMedina <brian.medina001@albright.edu>
2026-09-06 18:27:19 -0400

    10th commit

---
+++ assignments/02_variables_and_data_types/02_11_adding_comments.py
@@ -0,0 +1,11 @@
+invintation_guest = "Eric Pumpkins"
+print(f"You have been invited to my party, {invintation_guest}")
+#I'm pretty sure this is how some websites automate "fill in the blanks" sections.
+
+
+persons_name = "Jake Corners"
+
+print(persons_name.upper())
+print(persons_name.lower())
+print(persons_name.title())
+#I wonder what other kind for formating options like this there are.
